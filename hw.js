dfc4c01acccd6782c58d8fdbe691e58d720d09fa
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-03-08 16:26:59 +0100

    onclick
    pictures are loading button is changed but the hide function isnt running

--- hw.js
+++ hw.js
@@ -17,15 +17,15 @@ let loadImages = (query) => {
         let cards = document.getElementsByClassName("card");
   
         for (let i = 0; i < cards.length; i++) {
-          cards[i].firstElementChild.remove(); //removes svg
+          cards[i].firstElementChild.remove(); 
           let img = document.createElement("img");
           img.src = body.photos[i].src.large;
           img.className = "bd-placeholder-img card-img-top";
           img.style = "height: 200px; object-fit: cover";
   
-          cards[i].insertBefore(img, cards[i].firstChild); //adds the new image before other content
+          cards[i].insertBefore(img, cards[i].firstChild); 
   
-          // EX5 replaces 9 min with img id
+      
           cards[i].querySelector("small").innerText = "ID: " + body.photos[i].id;
         }
       });
@@ -34,7 +34,7 @@ let loadImages = (query) => {
   let loadOtherImages = (query) => {
     let hasImage = document.querySelector(".card").querySelector("img");
   
-    //we check for the existance of imgs instead of svgs because this function uses a different approach
+  
     if (hasImage) {
       fetch("https://api.pexels.com/v1/search?query=" + query, {
         method: "GET",
@@ -48,13 +48,16 @@ let loadImages = (query) => {
         .then((body) => {
           let cards = document.getElementsByClassName("card");
           for (let i = 0; i < cards.length; i++) {
-            cards[i].firstElementChild.src = body.photos[i].src.large; //switching the pre-existing image with a new image source
+            cards[i].firstElementChild.src = body.photos[i].src.large; 
             cards[i].querySelector("small").innerText =
               "ID: " + body.photos[i].id;
           }
         });
     } else {
-      // if cards still have svg we need the first method to handle the change
-      loadImages("sea"); // the parameter "sea" changes the default value of query
+ 
+      loadImages("sea");
     }
-  };
+
+};
+
+
